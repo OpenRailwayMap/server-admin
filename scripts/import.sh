de99ec16ef1f4ff75d268c51ca6e8742e4217afa
Michael Reichert <nakaner@gmx.net>
2019-11-16 12:14:01 +0100

    fix variable name

--- scripts/import.sh
+++ scripts/import.sh
@@ -24,7 +24,7 @@ if [ "$OSM2PGSQL_USE_FLATNODES" -eq 1 ]; then
 else
     FLATNODES_OPTION=""
 fi
-$OSM2PGSQL -d $DATABASE --merc --multi-geometry --hstore --style $OSM2PGSQL_STYLE --tag-transform $OSM2PGSQL_LUA --expire-tiles $EXPIRE_TILES_ZOOM --expire-output $EXPRIE_OUTPUT --expire-bbox-size 30000 --cache 12000 --slim $FLATNODES_OPTION $PLANET_FILTERED
+$OSM2PGSQL -d $DATABASE_NAME --merc --multi-geometry --hstore --style $OSM2PGSQL_STYLE --tag-transform $OSM2PGSQL_LUA --expire-tiles $EXPIRE_TILES_ZOOM --expire-output $EXPRIE_OUTPUT --expire-bbox-size 30000 --cache 12000 --slim $FLATNODES_OPTION $PLANET_FILTERED
 
 REPLICATION_TIMESTAMP=$($OSMIUM fileinfo -g header.option.osmosis_replication_timestamp $PLANET_FILE)
 echo "replication timestamp is $REPLICATION_TIMESTAMP"
